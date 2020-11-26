c2cfc0cd1de37f930253fbf9a76a2f44867b8d58
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2020-11-26 11:20:27 +0100

    add base config to json

---
+++ backend/base/nc.base.json.config.php
@@ -0,0 +1,35 @@
+<?php 
+
+    require_once dirname(__FILE__) . '/nc.base.file.json.php';
+
+    class nc_base_json_config {
+        static public function load() {
+
+            $fle_content = file_get_contents('json/nc.main.infos.jsonc');
+            $array = json_decode($fle_content);
+
+            self::parse_out($array, "NC");
+
+            die();
+        }
+        static function parse($key, $item, $add = "") {
+            $var = strtoupper($add . "_$key");
+            $var = str_replace("-", "_", $var);
+
+            define($var, $item);
+        }
+        static function parse_out($array, $add = "") {
+            foreach($array as $key => $item) {
+                if(is_string($item))
+                    self::parse($key, $item, $add);
+                else {
+                    self::parse_out($item, $add."_$key");
+                }
+            }
+        }
+
+    };
+
+
+
+?>
