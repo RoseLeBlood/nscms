51ede3e58a69a377a87f2353a20fbb889b114e48
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2020-11-29 15:50:20 +0100

    update version

--- logic/backend/config/nc.config.version.php
+++ logic/backend/config/nc.config.version.php
@@ -2,8 +2,8 @@
 
 define('NC_VERSION_MAYOR', 0, true);
 define('NC_VERSION_MINOR', 51, true);
-define('NC_VERSION_DEBUG', 0, true);
+define('NC_VERSION_DEBUG', 324, true);
 
-define('NC_VERSION_STRING', '0.51.0', true);
+define('NC_VERSION_STRING', '0.51.324', true);
     
 ?>
