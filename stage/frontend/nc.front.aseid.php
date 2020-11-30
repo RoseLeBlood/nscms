08465b3704fc737b51ccad68d2e889677a10064d
Amber-Sophia Schroeck <ambersophia.schroeck@outlook.de>
2020-11-30 15:35:07 +0100

    aside object load widgets from menu json

--- stage/frontend/nc.front.aseid.php
+++ stage/frontend/nc.front.aseid.php
@@ -1,15 +1,20 @@
 <?php 
 
     require_once dirname(__FILE__) . '/../backend/base/nc.base.visual.site.mod.php';
+    require_once dirname(__FILE__) .  '/logic/nc.frontend.menus.php';
 
     class nc_front_aseid extends nc_visual_site_mod {
         public function __construct($parent) {
             nc_visual_site_mod::__construct("nc.page.aside.htm", $parent);
 
-            // TODO: This part set in json file
-            //$this->add_module("NC_WIDGET_ONE", new nc_module_visual_bright("about-view-wigdet") );
-            //$this->add_module("NC_WIDGET_TWO", new nc_module_visual_bright("archiv-view-wigdet") );
-            $this->add_module("NC_WIDGET_THREE", new nc_module_visual_bright("Link-view-wigdet", $this) );
+            $inst = nc_frontend_menus_db::instance()->make("aside");
+            
+            foreach($inst as $item) {
+                $content = $item->get_content();
+                $var = $item->get_variable();
+
+                $this->add_module($var,  new nc_module_visual_bright($content, $this) );
+            }
         }
     };
 
--- stage/module.inc/nc.module.base.db.php
+++ stage/module.inc/nc.module.base.db.php
@@ -1,46 +1,14 @@
 <?php
+    require_once dirname(__FILE__) . '/../backend/base/nc.base.db.php';
 
-    class nc_module_base_db {
-        public function __construct($db, $assoc = true, $depth = 30, $options = 0) {
-            $this->m_strPath = NC_BASE_DIR . NC_TABLE_PATH . $db;
-            $this->m_arrayItems = array();
-
-            $this->parse($assoc, $depth, $options);
+    class nc_module_base_db extends nc_base_db {
+        public function __construct($directory, $assoc = true, $depth = 30, $options = 0) {
+            nc_base_db::__construct($directory, $assoc, $depth, $options);
         }
-        
-        public function get_path() {
-            return $this->m_strPath;
+        protected function add2array($file, $data) {
+            $array = ["file"=>  $file, 
+                      "data" => $data ];
+            array_push($this->m_arrayItems, $array);
         }
-        public function get_array() {
-            return $this->m_arrayItems;
-        }
-        public function get($id) {
-            return $this->m_arrayItems[$id];
-        }
-        public function count() {
-            return count($this->m_arrayItems);
-        }
-
-
-        private function parse($assoc = true, $depth = 30, $options = 0) {
-            $fileList = array_diff(scandir($this->m_strPath), [".", ".."]);
-
-            foreach($fileList as $filename) {
-                if(!is_dir ($filename) ) {
-                    if(pathinfo($filename, PATHINFO_EXTENSION) == "json") {
-                        $fle_content = file_get_contents($this->m_strPath."/$filename" );
-                        $jsonDecode = json_decode($fle_content, $assoc, $depth, $options);
-
-                        $array = ["file"=>  $this->m_strPath."/$filename", 
-                                  "data" => $jsonDecode ];
-
-                        array_push($this->m_arrayItems, $array);
-                    }
-                }
-            }
-        }
-
-        protected $m_arrayItems;
-        protected $m_strPath;
     };
 ?>
